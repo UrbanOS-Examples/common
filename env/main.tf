23f3f910bdee81ad8ef6a4bd7f3c8f9c6dced8e7
Johnson Denen <jdenen@pillartechnology.com>
2018-05-31 09:21:53 -0400

    SMRT-96 Add dev environment

---
+++ env/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "scos-alm-terraform-state"
+    key            = "vpc"
+    region         = "us-east-2"
+    dynamodb_table = "terraform_lock"
+    encrypt        = true
+  }
+}
+
+resource "aws_key_pair" "cloud_key" {
+  key_name   = "${terraform.workspace}_cloud_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRAvH6k7iEeRDz9SQtkH1o8KiUaed/e2hmzTUjA8bhmeWVLPsgCMKIUKR0jdPlvdZ0AmMLXswobsXj08dPgWxUZxoAWIqKCjd969KckydUpBtcb+X2Q+tpOcugBOQSY1H8hgNrdcRKEaUllTfvseJ9pBOYU7j9VuZ608HQhfZw7+aS8wi9o/BJwejtpWdlo6gkxXoIRqDX/ioYg+W6Tc7yoUzAEANwZAy3/3GKWDrh+9jnzR6mEEN48Nuee49wWfP5G0T/v4+Gvux5zioHb3rcmmR9YTkFOiv1poInhXlPdc7Q38yj+z6E+hACNN3rK80YjU0ByaSPltPjqm9ZYmPX"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "1.32.0"
+
+  name = "${var.vpc_name}"
+  cidr = "${var.vpc_cidr}"
+  azs  = "${var.vpc_azs}"
+
+  private_subnets = "${var.vpc_private_subnets}"
+  public_subnets  = "${var.vpc_public_subnets}"
+
+  enable_nat_gateway = "${var.vpc_enable_nat_gateway}"
+  single_nat_gateway = "${var.vpc_single_nat_gateway}"
+
+  enable_vpn_gateway       = "${var.vpc_enable_vpn_gateway}"
+  enable_s3_endpoint       = "${var.vpc_enable_s3_endpoint}"
+  enable_dynamodb_endpoint = "${var.vpc_enable_dynamodb_endpoint}"
+  enable_dns_hostnames     = "${var.vpc_enable_dns_hostnames}"
+
+  tags = {
+    Owner       = "${var.owner}"
+    Environment = "${var.environment}"
+    Name        = "${var.vpc_name}"
+  }
+}
+
---
+++ env/variables/dev.tfvars
@@ -0,0 +1,3 @@
+vpc_name = "dev"
+vpc_single_nat_gateway = true
+environment = "dev"
