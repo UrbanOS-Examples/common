77007050d844f89151b194734cb9906711fb8752
Kyle Smith <kylesmith@pillartechnology.com>
2018-06-06 11:15:21 -0400

    consolidate everything into one set of terraform scripts

--- nexus/efs.tf
+++
@@ -1,7 +0,0 @@
-module "efs" {
-  source = "../modules/efs"
-
-  efs_name = "${var.efs_name}"
-  efs_mode = "${var.efs_mode}"
-  efs_encrypted = "${var.efs_encrypted}"
-}
--- nexus/variables.tf
+++ nexus/variables.tf
@@ -1,3 +1,26 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-2"
+}
+# ------------- EFS -----------------------------
+variable "efs_name" {
+  description = "EFS name"
+  default = "nexus"
+}
+
+variable "efs_mode" {
+  description = "xfer mode:  generalPurpose OR maxIO"
+  default = "generalPurpose"
+}
+
+variable "efs_encrypted" {
+  description = "Is EFS encrypted?  true/false"
+  type = "string"
+  default = true
+}
+variable "environment" {
+  description = "Environment"
+}
 variable "component" {
   description = "The component this cluster will contain"
   default = "nexus"
