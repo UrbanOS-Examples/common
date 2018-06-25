2374492d0de580849889d1dcb5794947fa88a242
Johnson Denen <jdenen@pillartechnology.com>
2018-06-25 13:43:21 -0400

    SMRT-201 Use the correct role

--- scripts/zero_elb_count.sh
+++ scripts/zero_elb_count.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 function set_session {
-    local role_json=$(aws sts assume-role --role-arn arn:aws:iam::073132350570:role/admin_role --role-session-name JenkinsWaitForInfra)
+    local role_json=$(aws sts assume-role --role-arn arn:aws:iam::073132350570:role/jenkins_role --role-session-name JenkinsWaitForInfra)
     export AWS_ACCESS_KEY_ID=$(echo $role_json     | jq -r '.Credentials.AccessKeyId')
     export AWS_SECRET_ACCESS_KEY=$(echo $role_json | jq -r '.Credentials.SecretAccessKey')
-    export AWS_SECURITY_TOKEN=$(echo $role_json     | jq -r '.Credentials.SessionToken')
+    export AWS_SESSION_TOKEN=$(echo $role_json     | jq -r '.Credentials.SessionToken')
 }
 
 function get_elb_number {
