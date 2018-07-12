9a29764399220c91f232e37c302fdd41b58c5a59
Alex Markley <amarkley@pillartechnology.com>
2018-07-12 17:10:15 -0400

    SMRT-254 tag vpc and subnets appropriately for EKS
    SMRT-254 update documentation to reflect improvements

--- env/vpc.tf
+++ env/vpc.tf
@@ -18,10 +18,10 @@ module "vpc" {
   enable_dns_hostnames     = "${var.vpc_enable_dns_hostnames}"
 
   tags = {
-    Owner                                  = "${var.owner}"
-    Environment                            = "${var.environment}"
-    Name                                   = "${var.vpc_name}"
-    "kubernetes.io/cluster/streaming-kube" = "owned"
+    Owner                  = "${var.owner}"
+    Environment            = "${var.environment}"
+    Name                   = "${var.vpc_name}"
+    "kubernetes.io/cluster/${var.kubernetes_cluster_name}" = "shared"
   }
 }
 
