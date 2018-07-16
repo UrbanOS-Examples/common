5ce128e234e3f45200ba268a927715071d8aacfa
Kyle Smith <kylesmith@pillartechnology.com>
2018-07-16 15:25:51 -0400

    move ecr module from alm/ecr to modules/ecr

--- alm/container_registry.tf
+++ alm/container_registry.tf
@@ -1,19 +1,19 @@
 module "joomla_restore_repository" {
-  source = "ecr"
+  source = "../modules/ecr"
 
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "joomla-restore"
 }
 
 module "joomla_cron_repository" {
-  source = "ecr"
+  source = "../modules/ecr"
 
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "joomla-cron"
 }
 
 module "joomla_nginx_repository" {
-  source = "ecr"
+  source = "../modules/ecr"
 
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "joomla-nginx"
