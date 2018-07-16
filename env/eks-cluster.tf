d26d0453c71251a567c13b205957a389a4b65f4b
Alex Markley <amarkley@pillartechnology.com>
2018-07-16 13:55:24 -0400

    SMRT-255: make jarred happy

--- env/eks-cluster.tf
+++ env/eks-cluster.tf
@@ -12,9 +12,9 @@ module "eks-cluster" {
 
   worker_groups       = [ {
     name                    = "Workers"
-    asg_desired_capacity    = "4"
+    asg_desired_capacity    = "3"
     asg_min_size            = "1"
-    asg_max_size            = "4"
+    asg_max_size            = "6"
     instance_type           = "t2.medium"
   } ]
 
