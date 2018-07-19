00154d1d7fd098a434ca1eeefac6348445234f04
Johnson Denen <jdenen@pillartechnology.com>
2018-07-19 14:01:03 -0400

    SMRT-201 Fix small scripting issues
    co-authored-by: Alex Markley <amarkley@pillartechnology.com>

--- scripts/zero_elb_count.sh
+++ scripts/zero_elb_count.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 
-function set_session {
-  local role_json=$(aws sts assume-role --role-arn arn:aws:iam::073132350570:role/jenkins_role --role-session-name JenkinsWaitForInfra)
-  export AWS_ACCESS_KEY_ID=$(echo $role_json     | jq -r '.Credentials.AccessKeyId')
-  export AWS_SECRET_ACCESS_KEY=$(echo $role_json | jq -r '.Credentials.SecretAccessKey')
-  export AWS_SESSION_TOKEN=$(echo $role_json     | jq -r '.Credentials.SessionToken')
+environment=$1
+
+function help {
+    echo "Usage: $1 ENVIRONMENT"
 }
 
+if [ ! $environment ]; then help; exit 1; fi
+
+role_arn=$(grep role_arn backends/${environment}.conf | awk '{ print $3; }' | sed -e 's/^"//' -e 's/"$//')
+region=$(grep region backends/${environment}.conf | awk '{ print $3; }' | sed -e 's/^"//' -e 's/"$//')
+
+awsconfig=$(mktemp)
+
+cat << EOF >> ${awsconfig}
+[default]
+region = ${region}
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+role_arn = ${role_arn}
+EOF
+
+export AWS_CONFIG_FILE=${awsconfig}
+
 function get_elb_number {
-  local elb_json=$(aws elb describe-load-balancers --region us-east-2)
+  local elb_json=$(aws elb describe-load-balancers)
   echo $elb_json | jq '.LoadBalancerDescriptions | length'
 }
 
-set_session
-(($(get_elb_number) == 0))
+retval=(($(get_elb_number) == 0))
+
+rm -f ${awsconfig}
+
+exit ${retval}
