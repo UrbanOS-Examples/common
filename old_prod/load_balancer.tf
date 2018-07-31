b87382b565d66ebf4119298cc8d1eafaacd423b4
Brandon Cromer <bcromer@pillartechnology.com>
2018-07-31 15:00:16 -0400

    SMRT-276 Fixed some environment variable issues

--- old_prod/load_balancer.tf
+++ old_prod/load_balancer.tf
@@ -1,3 +1,8 @@
+locals {
+  url_subdomain_by_env = "${terraform.workspace == "prod" ? "" : terraform.workspace}"
+  cert_env_prefix      = "${local.url_subdomain_by_env == "" ? "" : "${local.url_subdomain_by_env}."}"
+}
+
 module "load_balancer" {
   source              = "../modules/old_prod_load_balancer"
   target_group_prefix = "${var.target_group_prefix}-Internal"
@@ -7,6 +12,7 @@ module "load_balancer" {
   subnet_ids          = "${data.aws_subnet.subnet.*.id}"
   is_external         = false
   is_enabled          = true
+  env_subdomain       = "${local.url_subdomain_by_env}"
 }
 
 module "load_balancer_external" {
@@ -18,13 +24,14 @@ module "load_balancer_external" {
   subnet_ids          = "${data.aws_subnet.subnet.*.id}"
   is_external         = true
   is_enabled          = "${var.alb_external}"
+  env_subdomain       = "${local.url_subdomain_by_env}"
 }
 
 resource "aws_acm_certificate" "load_balancer" {
-  domain_name       = "*.dev.smartcolumbusos.com"
+  domain_name       = "*.${local.cert_env_prefix}smartcolumbusos.com"
   validation_method = "DNS"
 
-  subject_alternative_names = ["dev.smartcolumbusos.com"]
+  subject_alternative_names = ["${local.cert_env_prefix}smartcolumbusos.com"]
 }
 
 variable "target_group_prefix" {
