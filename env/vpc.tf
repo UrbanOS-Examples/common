357c2216d03f750e7c4f4497fff382819d2f51ca
Remorc <bcromer07@gmail.com>
2018-08-15 16:13:24 -0400

    Revert "changed naming convention of eks kubeconfig"
    This reverts commit ea0f7eb117d442592aa94609926395af5e64eb81.

--- env/vpc.tf
+++ env/vpc.tf
@@ -56,16 +56,9 @@ resource "aws_security_group" "allow_all" {
   vpc_id      = "${module.vpc.vpc_id}"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
