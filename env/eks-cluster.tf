5ae54f227bfd3a56f1f2f4ea5f0ee538df54353e
Alex Markley <amarkley@pillartechnology.com>
2018-08-23 22:18:28 -0400

    switch to using our fork of terraform-aws-eks for now

--- env/eks-cluster.tf
+++ env/eks-cluster.tf
@@ -3,8 +3,9 @@ locals {
 }
 
 module "eks-cluster" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "1.3.0"
+  source = "github.com/SmartColumbusOS/terraform-aws-eks"
+  # source  = "terraform-aws-modules/eks/aws"
+  # version = "1.3.0"
 
   cluster_name = "${local.kubernetes_cluster_name}"
   subnets      = "${module.vpc.private_subnets}"
