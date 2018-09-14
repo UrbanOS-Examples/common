b26eb0a0abe448b7d63a41a992c26d15443a33ef
Christopher J. McClellan <cmcclellan@pillartechnology.com>
2018-09-14 08:04:21 -0400

    SMRT-436 Allow kubernetes through the Kong loadbalancer
    In order for Jupyter notebooks in non prod environments to hit the Kong
    through the loadbalancer, we need to allow ingress from the k8s workers.
    
    co-authored-by: Jeff Grunewald <jeff@grunewalddesign.com>

--- env/load_balancer.tf
+++ env/load_balancer.tf
@@ -26,12 +26,32 @@ resource "aws_security_group" "os_servers" {
   }
 }
 
+resource "aws_security_group" "allow_kubernetes_internet" {
+  name_prefix   = "Allow Kubernetes"
+  vpc_id = "${module.vpc.vpc_id}"
+  description = "Allows jupyter notebooks to access api gateway"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    security_groups = ["${module.eks-cluster.worker_security_group_id}"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    security_groups = ["${module.eks-cluster.worker_security_group_id}"]
+  }
+}
+
 module "load_balancer_private" {
   source              = "../modules/old_prod_load_balancer"
   target_group_prefix = "${terraform.workspace}-Int"
   vpc_id              = "${module.vpc.vpc_id}"
   certificate_arn     = "${module.tls_certificate.arn}"
-  security_group_ids  = ["${aws_security_group.os_servers.id}"]
+  security_group_ids  = ["${aws_security_group.os_servers.id}", "${aws_security_group.allow_kubernetes_internet.id}"]
   subnet_ids          = "${module.vpc.private_subnets}"
   is_external         = false
   dns_zone            = "${terraform.workspace}.${var.root_dns_zone}"
