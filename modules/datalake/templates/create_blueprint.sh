08728610578ab125285f896ae3d1a6d894a4032d
bbrewer-pillar <bbrewer@pillartechnology.com>
2018-10-31 14:51:15 -0400

    SMRT-651 - some tuning to make hive queries actually work
    co-authored-by: Tim Regan <tregan@pillartechnology.com>

--- modules/datalake/templates/create_blueprint.sh
+++ modules/datalake/templates/create_blueprint.sh
@@ -13,7 +13,8 @@ create-blueprint() {
     timeout 10 cb blueprint describe --name "${BLUEPRINT_NAME}" \
         || cb blueprint create from-file \
             --file ${BLUEPRINT_FILE} \
-            --name "${BLUEPRINT_NAME}"
+            --name "${BLUEPRINT_NAME}" \
+            --description "Created $(date)"
 }
 
 main "${@}"
