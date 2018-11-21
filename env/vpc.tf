0207e84307e3f3512b49812dd0c0d9c1d303bd47
Erin Moore <emoore@pillartechnology.com>
2018-11-21 09:38:21 -0500

    SMRT-705 EKS cluster security groups is created outside of EKS module
    to allow deploying of hadoop, load balancer, et cetera without deploying EKS
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>

--- env/vpc.tf
+++ env/vpc.tf
@@ -50,33 +50,6 @@ module "vpc" {
   }
 }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id      = "${module.vpc.vpc_id}"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_db_subnet_group" "default" {
   name        = "environment db ${terraform.workspace} subnet group"
   description = "DB Subnet Group"
