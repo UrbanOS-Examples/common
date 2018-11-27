aa12c53a00381e071b46c88da8344e7d38112126
Kyle Smith <kylesmith@pillartechnology.com>
2018-11-27 10:39:52 -0500

    Using outputs instead of referencing resources directly

--- env/zone_associations.tf
+++ env/zone_associations.tf
@@ -2,7 +2,7 @@ resource "null_resource" "create_zone_assocation" {
   provisioner "local-exec" {
     command = <<EOF
       aws route53 create-vpc-association \
-      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.aws_route53_zone.public_hosted_reverse_zone.zone_id} \
+      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.reverse_dns_zone_id} \
       --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id}
     EOF
   }
@@ -12,7 +12,7 @@ resource "null_resource" "associate_vpc_and_zone" {
   provisioner "local-exec" {
     command = <<EOF
       aws route53 associate-vpc-with-hosted-zone \
-      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.aws_route53_zone.public_hosted_reverse_zone.zone_id} \
+      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.reverse_dns_zone_id} \
       --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id} || echo "Association already exists"
     EOF
   }
@@ -22,7 +22,7 @@ resource "null_resource" "delete_zone_authorization" {
   provisioner "local-exec" {
     command = <<EOF
       aws route53 delete-vpc-association-authorization \
-      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.aws_route53_zone.public_hosted_reverse_zone.zone_id} \
+      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.reverse_dns_zone_id} \
       --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id} || echo "Association already exists"
     EOF
   }
