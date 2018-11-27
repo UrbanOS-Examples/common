18682e6963b338f45dcf89c0ee7df46324aaa920
Kyle Smith <kylesmith@pillartechnology.com>
2018-11-27 16:52:03 -0500

    Updating zone association to share alm rdns address with envs:

--- env/zone_associations.tf
+++ env/zone_associations.tf
@@ -1,29 +1,26 @@
-resource "null_resource" "create_zone_assocation" {
-  provisioner "local-exec" {
-    command = <<EOF
-      aws route53 create-vpc-association \
-      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.reverse_dns_zone_id} \
-      --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id}
-    EOF
-  }
+locals {
+  host_zone_acct = "199837183662" # ALM account where the reverse lookup zone resides
+  assume_role    = "jenkins_role"
+  zone_id        = "${data.terraform_remote_state.alm_remote_state.reverse_dns_zone_id}"
+  cli_flags      = "--hosted-zone-id ${local.zone_id} --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id}"
 }
 
-resource "null_resource" "associate_vpc_and_zone" {
-  provisioner "local-exec" {
-    command = <<EOF
-      aws route53 associate-vpc-with-hosted-zone \
-      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.reverse_dns_zone_id} \
-      --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id} || echo "Association already exists"
-    EOF
-  }
+module "create_vpc_association_authorization" {
+  source = "github.com/opetch/terraform-aws-cli-resource"
+
+  account_id      = "${local.host_zone_acct}" 
+  role            = "${local.assume_role}"
+  cmd             = "aws route53 create-vpc-association-authorization ${local.cli_flags}"
+  destroy_cmd     = "aws route53 delete-vpc-association-authorization ${local.cli_flags}"
 }
 
-resource "null_resource" "delete_zone_authorization" {
-  provisioner "local-exec" {
-    command = <<EOF
-      aws route53 delete-vpc-association-authorization \
-      --hosted-zone-id ${data.terraform_remote_state.alm_remote_state.module.iam_stack.reverse_dns_zone_id} \
-      --vpc VPCRegion=${var.region},VPCId=${module.vpc.vpc_id} || echo "Association already exists"
-    EOF
-  }
+module "associate_vpc_with_zone" {
+  source = "github.com/opetch/terraform-aws-cli-resource"
+
+  # Uses the default provider account id if no account id is passed in
+  role            = "${local.assume_role}"
+  cmd             = "aws route53 associate-vpc-with-hosted-zone ${local.cli_flags}"
+  destroy_cmd     = "aws route53 disassociate-vpc-from-hosted-zone ${local.cli_flags}"
+
+  dependency_ids  = ["${module.create_vpc_association_authorization.id}"] 
 }
