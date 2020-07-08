2f1e20109e97ec281703cf7029144a8b499248dd
Benjamin Brewer <ben@benbrewer.me>
2020-07-08 12:47:54 -0400

    add dynamic thumbprint

---
+++ files/scripts/oidc_thumbprint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+thumbprint=$(
+  echo | \
+    openssl s_client -servername oidc.eks.${1}.amazonaws.com -connect oidc.eks.${1}.amazonaws.com:443 2>&- | \
+    sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | \
+    openssl x509 -fingerprint -noout | \
+    sed 's/://g' | awk -F= '{print $2}'
+)
+thumbprint_json="{\"thumbprint\": \"${thumbprint}\"}"
+
+echo $thumbprint_json
