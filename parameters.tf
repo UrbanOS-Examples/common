364e05accf6465e8542f8a12ef9f9a84a1ef3efb
srashee1 <saad.a.rasheed@accenture.com>
2021-09-13 09:56:23 -0500

    Adding kubeconfig parameters to param store
    co-authored-by: Kyle Smith <kyle.r.smith@accenture.com>

--- parameters.tf
+++ parameters.tf
@@ -11,21 +11,35 @@ resource "aws_ssm_parameter" "certificate" {
 
 resource "aws_ssm_parameter" "public_subnets" {
   name        = "${terraform.workspace}_public_subnets"
-  description = "Certificate ARN for Ingress"
+  description = "Public subnets for Ingress ALB"
   type        = "String"
   value       = join("\\,", module.vpc.public_subnets)
 }
 
 resource "aws_ssm_parameter" "security_groups" {
   name        = "${terraform.workspace}_security_group_id"
-  description = "AWS Security Groups ID"
+  description = "Allow all inbound security groups"
   type        = "String"
   value       = aws_security_group.allow_all.id
 }
 
 resource "aws_ssm_parameter" "eks_wafv2_web_acl_arn" {
   name        = "${terraform.workspace}eks_cluster_arn"
-  description = "AWS Security Groups ID"
+  description = "ARN for the EKS Cluster"
   type        = "String"
   value       = aws_wafv2_web_acl.eks_cluster.arn
+}
+
+resource "aws_ssm_parameter" "eks_cluster_endpoint" {
+  name        = "${terraform.workspace}_eks_cluster_endpoint"
+  description = "AWS EKS Cluster Endpoint URL"
+  type        = "String"
+  value       =  
+}
+
+resource "aws_ssm_parameter" "eks_cluster_cert_auth_data" {
+  name        = "${terraform.workspace}_eks_cluster_cert_auth_data"
+  description = "AWS EKS Certificate Authority Data"
+  type        = "String"
+  value       = 
 }
